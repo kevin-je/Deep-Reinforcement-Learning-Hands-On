cc56ab9f9dbb5c8e6c86776b866c0f1e29a8f68a
kevin-je <kevin_je@qq.com>
2026-09-14 13:34:27 +0800

    Initial commit

--- Chapter_04/01_CE_Method_on_CartPole.py
+++ Chapter_04/01_CE_Method_on_CartPole.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 import gymnasium as gym
 import numpy as np
@@ -14,8 +14,9 @@ from tqdm import tqdm
 # 模型超参数
 HIDDEN_SIZE = 128
 BATCH_SIZE = 16
-EPOCHS = 10
-PERCENTAGE = 30
+NUM_ITERATIONS = 36
+PERCENTAGE = 70
+LEARNING_RATE = 3e-3
 
 # 环境超参数
 NUM_EPISODES = 32
@@ -57,59 +58,60 @@ class StepDataset(Dataset):
     def __len__(self) -> int:
         return len(self.episodes_steps)
 
-    def __getitem__(self, idx: int) -> EpisodeStep:
-        return self.episodes_steps[idx]
+    def __getitem__(self, idx: int) -> Tuple[np.ndarray, int]:
+        return self.episodes_steps[idx].observation, self.episodes_steps[idx].action
 
 
 def play(net: nn.Module,
          env: gym.Env,
-         num_episodes: int = NUM_EPISODES
 ) -> Episode:
-
-    sm = nn.Softmax(dim=0)
 
     episode = Episode(step=[], reward=0.0)
 
     net.eval()
     with torch.no_grad():
 
-        for _ in range(num_episodes):
+        observation, _ = env.reset()
+        while True:
+            # 神经网络预测动作
+            action_prob_1 = torch.sigmoid(net(torch.tensor(observation, device=DEVICE))).item()
+            action = int(np.random.choice([0.0, 1.0], p=[1-action_prob_1, action_prob_1]))
 
-            observation, _ = env.reset()
+            next_observation, reward, terminated, truncated, _ = env.step(action)
+            env.render()
 
-            while True:
-                # 神经网络预测动作
-                action_prob = sm(net(torch.Tensor(observation).unsqueeze(0).to(DEVICE))[0]).to("cpu").numpy()
-                action = int(np.random.choice(np.arange(len(action_prob)), p=action_prob))
+            episode.step.append(EpisodeStep(observation=observation, action=action))
+            episode.reward += reward
+            observation = next_observation
 
-                next_observation, reward, terminated, truncated, _ = env.step(action)
-                env.render()
+            if terminated or truncated:
+                break
 
-                episode.step.append(EpisodeStep(observation=observation, action=action))
-                episode.reward += reward
-                observation = next_observation
-
-                if terminated or truncated:
-                    break
-
-    env.close()
     return episode
 
 if __name__ == "__main__":
     # 创建环境
     env = gym.make("CartPole-v1", render_mode = "human" if RENDER else None)
-    # 创建网络
-    net = Net(input_size=4, output_size=2).to(DEVICE)
 
-    for epoch in range(1, EPOCHS + 1):
-        print(f"------ Episode {epoch:03d} ------")
+    # 创建网络
+    net = Net(input_size=4, output_size=1).to(DEVICE)
+
+    # 创建标准与优化器
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
+
+    for iteration in range(1, NUM_ITERATIONS + 1):
+        print(f"------ ITERATION {iteration:03d} ------")
 
         # 获取观测数据
         print("游戏中......")
         episodes: List[Episode] = []
 
-        for _ in tqdm(range(NUM_EPISODES), colour="green"):
+        loops = tqdm(range(NUM_EPISODES), colour="green")
+        for _ in loops:
             episode = play(net=net, env=env)
+            loops.set_postfix({"reward": episode.reward})
             episodes.append(episode)
 
         # 构建 elite_episodes
@@ -122,3 +124,22 @@ if __name__ == "__main__":
         # 创建数据集
         step_dataset: Dataset = StepDataset(elite_episodes)
         dataloader: DataLoader = DataLoader(step_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+        loops = tqdm(dataloader, colour="green")
+        for batch_idx, (obs, action) in enumerate(loops, start=1):
+            loops.set_description(f"Batch: {batch_idx:03d}")
+
+            obs, action = obs.to(device=DEVICE), action.to(dtype=torch.float32, device=DEVICE)
+            action_prob = net(obs).squeeze(-1)
+            loss = criterion(action_prob, action)
+            loops.set_postfix({"loss": loss.item()})
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # 保存模型权重及优化器
+        torch.save(net.state_dict(), "./01_Weights.pth")
+        torch.save(optimizer.state_dict(), "./01_Optimizers.pth")
+
+    env.close()
