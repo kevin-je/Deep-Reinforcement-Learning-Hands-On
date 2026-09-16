bafab2ca3d42b9c5c3d81832bc063f88af61117e
kevin_je <kevin_je@qq.com>
2026-09-16 20:08:13 +0800

    Initial commit

--- Chapter_06/01_DQN_on_Pong.py
+++ Chapter_06/01_DQN_on_Pong.py
@@ -12,15 +12,13 @@ from torch import optim
 import cv2 as cv
 
 import os
+from copy import copy
 
 from dataclasses import dataclass
 
 from collections import deque
 
 from typing import Tuple, List
-
-from torchvision.datasets import flickr
-from tqdm import tqdm
 
 import json
 
@@ -29,11 +27,11 @@ import json
 LEARNING_RATE = 2e-4
 BATCH_SIZE = 32
 
-BUFFER_SIZE = 10_000
+BUFFER_SIZE = 1_000
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-EPSILON_START = 0.2
+EPSILON_START = 0.99
 EPSILON_END = 0.05
 DECAY_STEPS = 150_000
 
@@ -71,41 +69,41 @@ class DQN(nn.Module):
         self.sequential_ff = nn.Sequential(linear1, relu, linear2)
 
     def forward(self, obs: torch.Tensor) -> torch.Tensor:
+
+        assert obs.dtype is torch.uint8
         return self.sequential_ff(self.sequential_conv(obs/255.0))
 
 
 @dataclass
 class Transition:
-    obs_stack: torch.Tensor
+    obs_stack: deque[np.ndarray]
     action: int
     reward: int | float
-    next_obs_stack: torch.Tensor
+    next_obs_stack: deque[np.ndarray]
     done: bool
 
 
+@torch.no_grad()
 def play_single_step(
         env: gym.Env,
-        obs_stack: torch.Tensor,
+        obs_stack: deque[np.ndarray],
         dqn: DQN,
-        dqn_tgt: DQN,
         num_steps: int
 ) -> Tuple[int, float, np.ndarray, bool]:
 
-    # 如果条件满足，将 dqn 的权重复制到 dqn_tgt
-    if num_steps % TGT_UPDATE_FREQ == 0:
-        dqn_tgt.load_state_dict(dqn.state_dict())
-
-
-    if obs_stack.size(0) < NUM_FRAMES:
+    if len(obs_stack) < NUM_FRAMES:
         action: int = env.action_space.sample()
 
     else:
         # 神经网络预测 Q 值
         actions_val: torch.Tensor = dqn(
             torch.as_tensor(
-                obs_stack,
-                dtype=torch.uint8,
-                device = DEVICE
+                np.asarray(
+                    obs_stack,
+                    dtype=np.uint8
+                ),
+                device=DEVICE,
+                dtype=torch.uint8
             ).unsqueeze(0)
         )
         # 计算 epsilon 的值
@@ -118,8 +116,6 @@ def play_single_step(
 
     # 将动作值输入环境
     next_obs, reward, terminated, truncated, _ = env.step(action)
-    assert type(next_obs) is np.ndarray
-    assert type(reward) is int
     done: bool = True if terminated or truncated else False
 
     # 渲染画面
@@ -129,64 +125,38 @@ def play_single_step(
     return action, reward, next_obs, done
 
 
-def play_single_episode(
+def init_reply_buffer(
         env: gym.Env,
-        dqn: DQN,
-        dqn_tgt: DQN,
-        num_steps: int,
-        replay_buffer: deque,
-        num_frames: int = NUM_FRAMES
-) -> Tuple[deque, int, float]:
+        replay_buffer: deque[Transition]
+) -> Tuple[deque[Transition], int]:
+    assert len(replay_buffer) == 0
 
-    obs_stack: deque = deque(maxlen=num_frames)       # [tensor(84, 84)]
-    next_obs_stack: deque = deque(maxlen=num_frames)
+    obs_stack = deque(maxlen=NUM_FRAMES)
+    obs, _ = env.reset()
+    next_obs_stack = deque(maxlen=NUM_FRAMES)
 
+    action = 0
+    reward: int = 0
+    done = False
     score = 0
 
-    # 初始化环境
-    obs, _ = env.reset()
-
-    while True:
+    for _ in range(NUM_FRAMES):
         obs_stack.append(obs)
-        obs_stack_t = torch.as_tensor(
-            np.asarray(obs_stack),
-            dtype=torch.uint8,
-            device=DEVICE
-        )
-
-        action, reward, next_obs, done = play_single_step(
-            env,
-            obs_stack_t,
-            dqn,
-            dqn_tgt,
-            num_steps
-        )
-        num_steps += 1
+        action = env.action_space.sample()
+        next_obs, reward, terminated, truncated, _ = env.step(action)
+        next_obs_stack.append(next_obs)
+        obs = next_obs
         score += reward
 
-        next_obs_stack.append(next_obs)
-
-        # 生成一条 transition
-        if len(obs_stack) == num_frames:
-            trans = Transition(
-                obs_stack_t,
-                action,
-                reward,
-                torch.as_tensor(
-                    np.asarray(next_obs_stack),
-                    dtype=torch.uint8
-                ),
-                done
-            )
-
-            # 将生成的转移放入回放缓冲区
-            replay_buffer.append(trans)
-
-        obs = next_obs
-
-        if done: break
-
-    return replay_buffer, num_steps, score
+    trans = Transition(
+        obs_stack=obs_stack,
+        action=action,
+        reward=reward,
+        next_obs_stack=next_obs_stack,
+        done=done
+    )
+    replay_buffer.append(trans)
+    return replay_buffer, score
 
 
 class ResizeImg(gym.ObservationWrapper):
@@ -196,26 +166,10 @@ class ResizeImg(gym.ObservationWrapper):
         self.img_size = img_size
 
     def observation(self, observation: np.ndarray) -> np.ndarray:
-        # img = Image.fromarray(obs)
-        # 转换为灰度图
-        # img = img.convert("L")
-        # 裁剪
-        # img = img.crop((0, 30, 160, 190))
         observation = observation[30:191, :161]
         # 缩放
         observation = cv.resize(observation, self.img_size)
         return observation
-
-
-def cal_q_val_tgt(dqn_tgt: DQN, next_obs_stack_t: torch.ByteTensor, gamma: float = GAMMA) -> float:
-    if trans.done:
-        q_val_tgt = trans.reward
-
-    else:
-        next_obs_stack = trans.next_obs_stack
-        q_val_tgt = dqn_tgt(next_obs_stack.unsqueeze(0)).max(dim=1).values.item() * gamma + trans.reward
-
-    return q_val_tgt
 
 
 def cal_epsilon(
@@ -226,22 +180,115 @@ def cal_epsilon(
     return max(epsilon_start - (step / decay_steps), epsilon_end)
 
 
-def get_samples(batch: List[Transition], dqn_tgt: DQN) -> Tuple[torch.ByteTensor, torch.FloatTensor]:
-    obs_stacks_t = []
-    actions_t = []
-    rewards_t =[]
+def get_replay_buffer(
+        env: gym.Env,
+        dqn: DQN,
+        replay_buffer: deque[Transition],
+        num_steps: int,
+        score: int
+) -> Tuple[deque[Transition], int, int]:
+
+    obs_stack = replay_buffer[-1].next_obs_stack
+    done = replay_buffer[-1].done
+
+    if done:
+        obs, _ = env.reset()
+        obs_stack.append(obs)
+        score = 0
+
+    action, reward, next_obs, done = play_single_step(env, obs_stack, dqn, num_steps)
+    num_steps += 1
+    score += reward
+
+    # 获取 next_obs_stack
+    next_obs_stack = copy(obs_stack)
+    next_obs_stack.append(next_obs)
+
+    # 生成一条 transition 并放入回放缓冲区
+    trans = Transition(
+        obs_stack = obs_stack,
+        action = action,
+        reward = reward,
+        next_obs_stack = next_obs_stack,
+        done = done
+    )
+    replay_buffer.append(trans)
+    return replay_buffer, num_steps, score
+
+
+@torch.no_grad()
+def get_samples(
+        batch: List[Transition],
+        dqn_tgt: DQN,
+        gamma: float = GAMMA
+) -> Tuple[torch.ByteTensor, torch.Tensor, torch.FloatTensor]:
+
+    obs_stacks = []
+    actions = []
+    rewards =[]
+    next_obs_stacks = []
+    dones = []
 
     for trans in batch:
-        obs_stacks_t.append(trans.obs_stack)
-        actions_t.append(trans.action)
-        rewards_t.append(trans.reward)
+        obs_stacks.append(trans.obs_stack)
+        actions.append(trans.action)
+        rewards.append(trans.reward)
+        next_obs_stacks.append(trans.next_obs_stack)
+        dones.append(trans.done)
+
+    # 转换成tensor
+    obs_stacks_t = torch.as_tensor(
+        np.asarray(obs_stacks),
+        device=DEVICE,
+        dtype=torch.uint8
+    )
+    actions_t = torch.as_tensor(
+        np.asarray(actions),
+        device=DEVICE,
+    )
+    rewards_t = torch.as_tensor(
+        np.asarray(rewards),
+        device=DEVICE
+    )
+    next_obs_stacks_t = torch.as_tensor(
+        np.asarray(next_obs_stacks, dtype=np.uint8),
+        device=DEVICE,
+        dtype=torch.uint8
+    )
+    dones_t = torch.as_tensor(
+        np.asarray(
+            dones,
+            dtype=np.bool
+        ),
+        device=DEVICE,
+        dtype=torch.bool
+    )
+    assert dones_t.dtype is torch.bool
+
+    # 计算 tgt_q_vals
+    tgt_q_vals_t = dqn_tgt(next_obs_stacks_t).max(dim=1).values * gamma + rewards_t
+    tgt_q_vals_t[dones_t] = rewards_t[dones_t]
+
+    return obs_stacks_t, actions_t, tgt_q_vals_t
 
 
+def cal_loss(
+        replay_buffer: deque[Transition],
+        dqn: DQN,
+        dqn_tgt: DQN,
+        criterion: nn.MSELoss
+) -> torch.Tensor:
 
+    # 获取样本
+    batch = random.sample(replay_buffer, BATCH_SIZE)
+    obs_stacks_t, actions_t, tgt_q_vals_t = get_samples(batch, dqn_tgt)
+
+    # 损失计算
+    q_vals_t: torch.Tensor = dqn(obs_stacks_t).gather(1, actions_t.unsqueeze(1))
+    loss = criterion(q_vals_t, tgt_q_vals_t)
+    return loss
 
 def train(env: gym.Env, dqn: DQN, dqn_tgt: DQN) -> None:
-    # 创建回放缓冲区
-    replay_buffer = deque(maxlen=BUFFER_SIZE)
 
     # 创建 criterion 和 optimizer
     criterion = nn.MSELoss()
@@ -254,65 +301,57 @@ def train(env: gym.Env, dqn: DQN, dqn_tgt: DQN) -> None:
     if os.path.exists("./01_Weights.pth"):
         dqn.load_state_dict(torch.load("./01_Weights.pth"))
 
-    if os.path.exists("./01_check_point.json"):
-        with open("./01_check_point.json", "r") as f:
+    if os.path.exists("./01_checkpoint.json"):
+        with open("./01_checkpoint.json", "r") as f:
             num_steps = json.load(f)["num_steps"]
-    else: num_steps: int = 0
+    else:
+        num_steps: int = 0
+
+    # 创建回放缓冲区
+    replay_buffer = deque(maxlen=BUFFER_SIZE)
+    # 先玩几步，初始化缓冲区
+    replay_buffer, score = init_reply_buffer(env, replay_buffer)
+
+    dqn_tgt.eval()
 
     episode_idx: int = 0
     while True:
-        # 开始游玩
-        print("Playing...")
+        # 开始正式游玩
         dqn.eval()
-        with torch.no_grad():
 
-            replay_buffer, num_steps, score = play_single_episode(
-                env,
-                dqn,
-                dqn_tgt,
-                num_steps,
-                replay_buffer
-            )
+        replay_buffer, num_steps, score = get_replay_buffer(env, dqn, replay_buffer, num_steps, score)
+        if num_steps % TGT_UPDATE_FREQ == 0:
+            dqn_tgt.load_state_dict(dqn.state_dict())
 
+        # 记录 episode 数
+        if replay_buffer[-1].done:
             episode_idx += 1
-            print(f"Episode: {episode_idx:06d}; Score: {score:3d}")
-            if score == SCORE_BOUNDARY:
+            print(f"Episode: {episode_idx:06d}; Score: {int(score):3d}")
+
+            if score >= SCORE_BOUNDARY:
                 print("Solved!")
                 break
 
+        if len(replay_buffer) == BUFFER_SIZE:
 
-        # 模型训练
-        print("Training...")
-        # 从回放缓冲区采样
-        batch = random.sample(replay_buffer, BATCH_SIZE)
-
-        dqn.train()
-        dqn = dqn.to(DEVICE)
-        loops = tqdm(dataloader, colour="green")
-        for batch_idx, (obs_stacks_t, q_vals_tgt, actions) in enumerate(loops, 1):
-            loops.set_description(f"Batch {batch_idx}")
-
-            q_vals = dqn(obs_stacks_t.to(DEVICE))
-            q_vals_tgt = q_vals_tgt.to(DEVICE)
-            actions = actions.to(DEVICE)
-
-            q_vals = q_vals.gather(1, actions.unsqueeze(1)).squeeze(1)
+            # 模型训练
+            dqn.train()
 
             # 计算损失
-            loss = criterion(q_vals, q_vals_tgt)
-            loops.set_postfix({"loss": loss.item()})
+            loss = cal_loss(replay_buffer, dqn, dqn_tgt, criterion)
+            print(f"loss: {loss.item():2.3f}")
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        # 保存模型权重、优化器和步数
-        torch.save(dqn.state_dict(), "./01_Weights.pth")
-        torch.save(optimizer.state_dict(), "./01_Optimizer.pth")
-        with open("./01_check_point.json", "w") as f:
-            json.dump({"num_steps": num_steps}, f)
+            # 保存模型权重、优化器和步数
+            torch.save(dqn.state_dict(), "./01_Weights.pth")
+            torch.save(optimizer.state_dict(), "./01_Optimizer.pth")
+            with open("./01_checkpoint.json", "w") as f:
+                json.dump({"num_steps": num_steps}, f)
 
-        print()
+            print()
 
 
 if __name__ == '__main__':
@@ -322,8 +361,7 @@ if __name__ == '__main__':
     env = ResizeImg(env, IMG_SIZE)
 
     dqn = DQN((NUM_FRAMES, *IMG_SIZE), env.action_space.n).to(DEVICE)
-    dqn_tgt = DQN((NUM_FRAMES, *IMG_SIZE), env.action_space.n)
-
+    dqn_tgt = DQN((NUM_FRAMES, *IMG_SIZE), env.action_space.n).to(DEVICE)
 
     train(env, dqn, dqn_tgt)
     env.close()
